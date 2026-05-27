e71a4815b5c98b74bca27cfb0273c7f9e778d5c3
psontay <phamsontay2206@gmail.com>
2026-05-27 23:23:09 +0700

    fix(account-test): marshal testcase's body for request

--- api/account.go
+++ api/account.go
@@ -10,7 +10,7 @@ import (
 )
 
 type createAccountRequest struct {
-	Owner    string `json:"owner" biding:"required"`
+	Owner    string `json:"owner" binding:"required,gt=0"`
 	Currency string `json:"currency" binding:"required,oneof=USD EUR VND"`
 }
 
