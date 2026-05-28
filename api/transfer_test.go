5c08e1e49329db1388870c0317a8343c18c9e83c
psontay <phamsontay2206@gmail.com>
2026-05-28 14:26:47 +0700

    feat(transfer-test): list & get

--- api/transfer_test.go
+++ api/transfer_test.go
@@ -6,9 +6,11 @@ import (
 	"bytes"
 	"database/sql"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang/mock/gomock"
@@ -196,6 +198,194 @@ func TestCreateTransferAPI(t *testing.T) {
 
 			request, err := http.NewRequest(http.MethodPost, "/transfers", bytes.NewReader(data))
 			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
+}
+
+func TestGetTransferAPI(t *testing.T) {
+	transfer := database.Transfer{
+		ID:            1,
+		FromAccountID: 1,
+		ToAccountID:   2,
+		Amount:        1,
+		CreatedAt:     time.Now(),
+	}
+	testCases := []struct {
+		name          string
+		transferID    int64
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:       "OK",
+			transferID: transfer.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetTransfer(gomock.Any(), gomock.Eq(transfer.ID)).Times(1).Return(transfer, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name:       "NotFound",
+			transferID: transfer.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetTransfer(gomock.Any(), gomock.Eq(transfer.ID)).Times(1).Return(database.Transfer{}, sql.ErrNoRows)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name:       "InternalError",
+			transferID: transfer.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetTransfer(gomock.Any(), gomock.Eq(transfer.ID)).Times(1).Return(database.Transfer{}, sql.ErrConnDone)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+		{
+			name:       "InvalidID",
+			transferID: 0,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().GetTransfer(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+	}
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			url := fmt.Sprintf("/transfers/%d", tc.transferID)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(recorder)
+		})
+	}
+}
+
+func TestListTransfersAPI(t *testing.T) {
+	n := 5
+	transfers := make([]database.Transfer, n)
+	for i := 0; i < n; i++ {
+		transfers[i] = database.Transfer{
+			ID:            int64(i + 1),
+			FromAccountID: 1,
+			ToAccountID:   2,
+			Amount:        10,
+			CreatedAt:     time.Now(),
+		}
+	}
+
+	testCases := []struct {
+		name          string
+		query         gin.H
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name: "OK",
+			query: gin.H{
+				"page_id":   1,
+				"page_size": 5,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := database.ListTransfersParams{
+					Limit:  5,
+					Offset: 0,
+				}
+				store.EXPECT().ListTransfers(gomock.Any(), gomock.Eq(arg)).Times(1).Return(transfers, nil)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+			},
+		},
+		{
+			name: "InvalidPageID",
+			query: gin.H{
+				"page_id":   -1,
+				"page_size": 5,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().ListTransfers(gomock.Any(), gomock.Any()).Times(0)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusBadRequest, recorder.Code)
+			},
+		},
+		{
+			name: "NotFound",
+			query: gin.H{
+				"page_id":   1,
+				"page_size": 5,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := database.ListTransfersParams{
+					Limit:  5,
+					Offset: 0,
+				}
+				store.EXPECT().ListTransfers(gomock.Any(), gomock.Eq(arg)).Times(1).Return([]database.Transfer{}, sql.ErrNoRows)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusNotFound, recorder.Code)
+			},
+		},
+		{
+			name: "InternalError",
+			query: gin.H{
+				"page_id":   1,
+				"page_size": 5,
+			},
+			buildStubs: func(store *mockdb.MockStore) {
+				arg := database.ListTransfersParams{
+					Limit:  5,
+					Offset: 0,
+				}
+				store.EXPECT().ListTransfers(gomock.Any(), gomock.Eq(arg)).Times(1).Return([]database.Transfer{}, sql.ErrConnDone)
+			},
+			checkResponse: func(recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
+
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
+
+			request, err := http.NewRequest(http.MethodGet, "/transfers", nil)
+			require.NoError(t, err)
+
+			q := request.URL.Query()
+			q.Add("page_id", fmt.Sprintf("%v", tc.query["page_id"]))
+			q.Add("page_size", fmt.Sprintf("%v", tc.query["page_size"]))
+			request.URL.RawQuery = q.Encode()
 
 			server.router.ServeHTTP(recorder, request)
 			tc.checkResponse(recorder)
