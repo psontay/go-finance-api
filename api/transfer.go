dbce8fe02ecab41769898e88fc74f5779a93d3aa
psontay <phamsontay2206@gmail.com>
2026-05-28 12:59:19 +0700

    feat(transfer-api): list transfers

--- api/transfer.go
+++ api/transfer.go
@@ -55,13 +55,39 @@ func (server *Server) getTransfer(ctx *gin.Context) {
 	transfer, err := server.store.GetTransfer(ctx, req.ID)
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
-			ctx.JSON(http.StatusBadRequest, errorResponse(err))
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
 			return
 		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
 	ctx.JSON(http.StatusOK, transfer)
+}
+
+type listServersRequest struct {
+	PageID   int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listServers(ctx *gin.Context) {
+	var req listServersRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	arg := database.ListTransfersParams{
+		Limit:  req.PageSize,
+		Offset: (req.PageID - 1) * req.PageSize,
+	}
+
+	transfers, err := server.store.ListTransfers(ctx, arg)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+	ctx.JSON(http.StatusOK, transfers)
 }
 
 func (server *Server) validAccount(ctx *gin.Context, accountID int64, currency string) bool {
