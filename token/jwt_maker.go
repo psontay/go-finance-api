36bc11340740b381f78010bc5834778bc520d3cb
psontay <phamsontay2206@gmail.com>
2026-05-29 15:31:36 +0700

    chore(maker): add payload for return maker

--- token/jwt_maker.go
+++ token/jwt_maker.go
@@ -28,10 +28,10 @@ func NewJWTMaker(secretKey string) (Maker, error) {
 	return &JWTMaker{secretKey}, nil
 }
 
-func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, error) {
+func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (string, *Payload, error) {
 	payload, err := NewPayload(username, duration)
 	if err != nil {
-		return "", err
+		return "", payload, err
 	}
 
 	// put payload in claims
@@ -46,7 +46,8 @@ func (maker *JWTMaker) CreateToken(username string, duration time.Duration) (str
 		},
 	}
 	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	return jwtToken.SignedString([]byte(maker.secretKey))
+	signedToken, _ := jwtToken.SignedString([]byte(maker.secretKey))
+	return signedToken, payload, nil
 }
 
 func (maker *JWTMaker) VerifyToken(token string) (*Payload, error) {
--- token/maker.go
+++ token/maker.go
@@ -4,7 +4,7 @@ import "time"
 
 type Maker interface {
 	// gen token for specific user
-	CreateToken(username string, duration time.Duration) (string, error)
+	CreateToken(username string, duration time.Duration) (string, *Payload, error)
 	// check token & get payload from it
 	VerifyToken(token string) (*Payload, error)
 }
